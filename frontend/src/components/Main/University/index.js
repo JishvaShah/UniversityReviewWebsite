0debca3eb71766bf963021ecb61756c44a264e7b
jiangting <jtting@126.com>
2023-11-10 15:23:36 -0800

    Add useState to update popularity

--- frontend/src/components/Main/University/index.js
+++ frontend/src/components/Main/University/index.js
@@ -3,14 +3,22 @@ import {Link} from "react-router-dom";
 import "./universityCard.css";
 
 const UniversityCard = ({university}) => {
+    const [popularity, setPopularity] = useState(university.popularity);
+    const [like, setLike] = useState(false);
 
 
-    const inList = (university) => {
-        // return user.favUniversityList.includes(university.id);
-    }
+
+    const likeUniversityHandler = (universityId, dispatch) => {
+        if (like) {
+            setPopularity(popularity -1);
+            setLike(!like);
 
 
-    const likeUniversityHandler = (university, dispatch) => {
+        } else {
+            setPopularity(popularity + 1);
+            setLike(!like);
+        }
+
         // if (!user.username || user.username === "") {
         //     alert("Please Login to like a university.")
         //     return;
@@ -31,16 +39,17 @@ const UniversityCard = ({university}) => {
     return (
         <div className="col">
             <div className="card mx-2" >
-                <div className="card-body" data-bs-toggle="modal" data-bs-target={`#modal-${university.id}`}>
+
                 <img src={university.photo} className="card-img-top wd-card-img" alt="sample"/>
                 <button className="btn btn-outline-primary wd-button wd-button-on-img"
-                        onClick={() => likeUniversityHandler(university.id, dispatch)}>
-                    <i className={`fas fa-heart ${inList(university) ? "wd-color-red" : ""}`}/>
+                        onClick={() => likeUniversityHandler(university.id)}>
+                    <i className={`fas fa-heart ${like ? "wd-color-red" : ""}`}/>
                 </button>
+                <div className="card-body" data-bs-toggle="modal" data-bs-target={`#modal-${university.id}`}>
                         <h5 className="wd-block-title">{university.title}</h5>
                         <p className="card-text">{university.description.substring(0, 50) + " ..."}</p>
                         <p className="card-text">Popularity: &nbsp;
-                            <i className="fas fa-heart red-color"/> {university.popularity}</p>
+                            <i className="fas fa-heart red-color"/> {popularity}</p>
                     </div>
 
                 <div className="modal fade" id={`modal-${university.id}`} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true"  >
@@ -61,7 +70,6 @@ const UniversityCard = ({university}) => {
 
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                {/*<button type="button" className="btn btn-primary">Save changes</button>*/}
                             </div>
                         </div>
                     </div>
