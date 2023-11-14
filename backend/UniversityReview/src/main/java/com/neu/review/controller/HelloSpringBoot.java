86aabe1cf1b665967a03a521cf90baa766f10bea
xue.jianh <joshaxue@github.khoury.northeastern.edu>
2023-11-14 11:03:04 -0800

    CORS

--- backend/UniversityReview/src/main/java/com/neu/review/controller/HelloSpringBoot.java
+++ backend/UniversityReview/src/main/java/com/neu/review/controller/HelloSpringBoot.java
@@ -1,10 +1,12 @@
 package com.neu.review.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HelloSpringBoot {
 
     @GetMapping("/hello")
--- backend/UniversityReview/src/main/java/com/neu/review/controller/UserController.java
+++ backend/UniversityReview/src/main/java/com/neu/review/controller/UserController.java
@@ -8,11 +8,13 @@ import com.neu.review.resp.*;
 import com.neu.review.service.UniversityService;
 import com.neu.review.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
