be5b69a40ab8dfd7f7dd1bdcd6c300aaa652d531
yxhuang999 <huang.yixua@northeastern.edu>
2023-11-19 00:24:56 -0800

    Add Tests for Service Classes

--- backend/UniversityReview/src/test/java/com/neu/review/service/FavouriteServiceTest.java
+++ backend/UniversityReview/src/test/java/com/neu/review/service/FavouriteServiceTest.java
@@ -1,4 +1,52 @@
 package com.neu.review.service;
 
+import com.neu.review.mapper.FavouriteMapper;
+import com.neu.review.pojo.Favourite;
+import com.neu.review.service.FavouriteService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
 public class FavouriteServiceTest {
+
+    @Autowired
+    private FavouriteService favouriteService;
+
+    @Autowired
+    private FavouriteMapper favouriteMapper;
+
+    @Test
+    void testCreateAndDelete() {
+        Favourite newFavourite = new Favourite(null, 1, 1);
+        favouriteService.create(newFavourite);
+        System.out.println("Created Favourite: " + newFavourite);
+
+        favouriteService.delete(newFavourite.getId());
+        System.out.println("Deleted Favourite with ID: " + newFavourite.getId());
+    }
+
+    @Test
+    void testGetByUserIDUniID() {
+        Favourite newFavourite = new Favourite(null, 2, 2);
+        favouriteService.create(newFavourite);
+
+        Favourite favourite = favouriteService.getByUserIDUniID(2, 2);
+        System.out.println("Fetched Favourite by UserID and UniID: " + favourite);
+
+        favouriteService.delete(newFavourite.getId());
+    }
+
+    @Test
+    void testGetByUserID() {
+        Favourite newFavourite = new Favourite(null, 3, 3);
+        favouriteService.create(newFavourite);
+
+        List<Favourite> favourites = favouriteService.getByUserID(3);
+        System.out.println("Fetched Favourites by UserID: " + favourites);
+
+        favouriteService.delete(newFavourite.getId());
+    }
 }
